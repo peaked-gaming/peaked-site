9557becbfb28e3f4a96c87a88500df3f05474872
devcarlan <devcarlan@gmail.com>
2019-02-05 02:30:53 -0500

    form functionality added. waiting on testing

---
+++ forms/contact.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST['submit'])) {
+  $name   = $_POST['name'];
+  $email   = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+
+
+  $mailTo = "";
+  $head = "From: ".$email;
+  $contact = "New Contact Form Submission From: ".$name.".\n\n".$message;
+
+  mail($mailTo, $subject, $txt, $head);
+  header("Location: index.php?contactformsubmit")
+}
